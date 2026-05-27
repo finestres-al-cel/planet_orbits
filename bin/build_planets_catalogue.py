7d8db61f2bf82cb75f866feb826969a5665c28af
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2026-05-27 17:05:36 +0200

    query horizon system to download data

--- bin/build_planets_catalogue.py
+++ bin/build_planets_catalogue.py
@@ -1,39 +1,47 @@
-from astropy.time import Time
-from astropy.coordinates import get_body, solar_system_ephemeris, BarycentricTrueEcliptic
-import astropy.units as u
-from datetime import datetime, timedelta
-import pandas as pd
-import numpy as np
+import argparse
+
+from planet_orbits.query_horizon import concatenate_queries, OBJIDS    
 
 def main(args):
-    # Define time range
-    dates = Time(np.arange(args.start_date, args.end_date, args.date_step, dtype="datetime64[D]"))
+    
+    planets = []
+    if args.planet.lower() == "all":
+        planets = list(OBJIDS.keys())
+    else:
+        planet = args.planet.lower()
+        if planet not in OBJIDS:
+            raise ValueError(f"Invalid target planet. Should be one of {list(OBJIDS.keys())} or 'all'.")
+        if planet == "sun":
+            planets = [planet]
+        else:
+            planets = ["sun", planet]
 
-    # Planets of interest
-    bodies = ["sun", "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+    dfs = []
+    for planet in planets:
+        print(f"Querying coordinates for {planet}...")
+        try:
+            df = concatenate_queries(
+                args.start_date, args.stop_date, args.step_size, OBJIDS[planet], break_times=args.break_times)
+        except RuntimeError as e:
+            print(f"Error occurred while querying {planet}: {e}")
+            print("Try again with a smaller break_time, or a smaller time range (start_date, stop_date).")
+            print("Skipping object.")
+            continue
+        except Exception as e:
+            print(f"An unexpected error occurred while querying {planet}: {e}")
+            print("Skipping object.")
+            continue
+        dfs.append(df)
 
-    # Dictionary to store the data
-    data = {"Date": [date.strftime('%Y-%m-%d') for date in dates.datetime]}
-
-    # Obtain J2000 equatorial coordinates
-    with solar_system_ephemeris.set("jpl"):
-        for body in bodies:
-            coords = get_body(body, dates)
-            ra = coords.icrs.ra.deg  # RA in degrees
-            dec = coords.icrs.dec.deg  # Dec in degrees
-
-            coords_ecliptic = coords.transform_to(BarycentricTrueEcliptic())
-            lon = coords_ecliptic.lon.deg
-            lat = coords_ecliptic.lat.deg
-
-            data[f"{body.capitalize()}_RA"] = ra
-            data[f"{body.capitalize()}_Dec"] = dec
-
-            data[f"{body.capitalize()}_lon"] = lon
-            data[f"{body.capitalize()}_lat"] = lat
-
-    # Create data frame
-    df = pd.DataFrame(data)
+    if len(dfs) == 0:
+        raise RuntimeError("No data was retrived (check logs)")
+    if len(dfs) == 1:
+        df = dfs[0]
+    else:
+        print("Merging data into a single dataframe...")
+        df = pd.merge(dfs[0], dfs[1], on="Date", how="outer")
+        for index in range(2, len(dfs)):
+            df = pd.merge(df, dfs[index], on="Date", how="outer")
 
     # Save data
     if args.filename.endswith(".fits") or args.filename.endswith(".fits.gz"):
@@ -41,24 +49,34 @@ def main(args):
     elif args.filename.endswith(".csv"):
         df.to_csv(args.filename, index=False) 
     else:
-        raise ValueError("File extension needs to be one of [.fits, .fits.gz .csv]")
+        raise ValueError("File extension needs to be one of [.fits, .fits.gz, .csv]")
     
 
     print(f"Dades desades a {args.filename}")
 
 
 if __name__ == "__main__":
-    import argparse
 
     # Default dates (15 years from today)
-    end_date_default = datetime.today().strftime('%Y-%m-%d')
-    start_date_default = (datetime.today() - timedelta(days=15*365)).strftime('%Y-%m-%d')
+    stop_time = '2026-06-20'
+    start_time = '1962-01-20'
+    step_size = '1h'
 
     parser = argparse.ArgumentParser(description="Build a catalogue of solar system bodies.")
     parser.add_argument("filename", type=str, help="Output file (fits or csv)")
-    parser.add_argument("--start-date", type=str, default=start_date_default, help="Start date (YYYY-MM-DD)")
-    parser.add_argument("--end-date", type=str, default=end_date_default, help="End date (YYYY-MM-DD)")
-    parser.add_argument("--date-step", type=int, default=1, help="Step in days")
-    
+    parser.add_argument("--start-date", type=str, default=start_time, help="Start date (YYYY-MM-DD)")
+    parser.add_argument("--stop-date", type=str, default=stop_time, help="Stop date (YYYY-MM-DD)")
+    parser.add_argument(
+        "--step-size", type=str, default=step_size, 
+        help="Time step. Expected format is 'Xu', where 'X' is an integer and 'u' are the units (d=days, h=hours, m=minutes)")
+    parser.add_argument(
+        "--break-times", type=str, default="10y", 
+        help="Maximum time between start_date and stop_date for each query. Expected format is 'Xu', where 'X' is an integer and 'u' are the units (d=days, m=months, y=years)")
+    parser.add_argument(
+        "--planet", type=str, default="mars", 
+        help=(f"Target planet for the catalogue. Should be one of {list(OBJIDS.keys())}. "
+              "To download coordinates for all planets, use 'all'. 'sun' will always be included."))
+
+
     args = parser.parse_args()
     main(args)
